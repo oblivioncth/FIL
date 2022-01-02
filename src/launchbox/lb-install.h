9b0d1726decd4045f2d25cd3ee7d492ff3e38efc
Christian Heimlich <chris@pcserenity.com>
2022-01-02 17:15:56 -0500

    Fix comment typo for LB::Install on playlist IDs

--- src/launchbox/lb-install.h
+++ src/launchbox/lb-install.h
@@ -73,7 +73,7 @@ private:
     QList<QString> mPurgableImages;
     QMap<QString, QString> mLinksToReverse;
     Qx::FreeIndexTracker<int> mLBDatabaseIDTracker = Qx::FreeIndexTracker<int>(0, -1, {});
-    // TODO: Even though the playlist game IDs dont seem to matter, at some for for completeness scann all playlists when hooking an install to get the
+    // TODO: Even though the playlist game IDs dont seem to matter, at some point for for completeness scan all playlists when hooking an install to get the
     // full list of in use IDs
 
 //-Constructor-------------------------------------------------------------------------------------------------
