1f06e493676d0f9144783c8af10aa2f8111597ee
Christian Heimlich <chris@pcserenity.com>
2022-01-21 19:52:57 -0500

    Add missing return type casts for ItemBuilder derivatives in Fe

--- src/frontend/fe-items.h
+++ src/frontend/fe-items.h
@@ -8,12 +8,12 @@ namespace Fe
 
 //-Class Forward Declarations---------------------------------------------------------------------------------------
 //class Item;
-//template <typename B, typename T, ENABLE_IF(std::is_base_of<Item, T>)> class ItemBuilder;
+//template <typename B, typename T, QX_ENABLE_IF(std::is_base_of<Item, T>)> class ItemBuilder;
 
 //-Namespace Global Classes-----------------------------------------------------------------------------------------
 class Item
 {
-    template <typename B, typename T, ENABLE_IF2(std::is_base_of<Item, T>)>
+    template <typename B, typename T, QX_ENABLE_IF_D(std::is_base_of<Item, T>)>
     friend class ItemBuilder;
 //-Instance Variables-----------------------------------------------------------------------------------------------
 private:
@@ -30,7 +30,7 @@ public:
     void transferOtherFields(QHash<QString, QString>& otherFields);
 };
 
-template <typename B, typename T, ENABLE_IF(std::is_base_of<Item, T>)>
+template <typename B, typename T, QX_ENABLE_IF(std::is_base_of<Item, T>)>
 class ItemBuilder
 {
 //-Instance Variables------------------------------------------------------------------------------------------
@@ -54,7 +54,7 @@ public:
 
 class BasicItem : public Item
 {
-    template <typename B, typename T, ENABLE_IF2(std::is_base_of<BasicItem, T>)>
+    template <typename B, typename T, QX_ENABLE_IF_D(std::is_base_of<BasicItem, T>)>
     friend class BasicItemBuilder;
 //-Instance Variables-----------------------------------------------------------------------------------------------
 protected:
@@ -72,19 +72,19 @@ public:
     QString getName() const;
 };
 
-template <typename B, typename T, ENABLE_IF(std::is_base_of<BasicItem, T>)>
+template <typename B, typename T, QX_ENABLE_IF(std::is_base_of<BasicItem, T>)>
 class BasicItemBuilder : public ItemBuilder<B, T>
 {
 //-Instance Functions------------------------------------------------------------------------------------------
 public:
-    B& wId(QString rawId) { ItemBuilder<B,T>::mItemBlueprint.mId = QUuid(rawId); return *this; }
-    B& wId(QUuid id) { ItemBuilder<B,T>::mItemBlueprint.mId = id; return *this; }
-    B& wName(QString name) { ItemBuilder<B,T>::mItemBlueprint.mName = name; return *this;}
+    B& wId(QString rawId) { ItemBuilder<B,T>::mItemBlueprint.mId = QUuid(rawId); return static_cast<B&>(*this); }
+    B& wId(QUuid id) { ItemBuilder<B,T>::mItemBlueprint.mId = id; return static_cast<B&>(*this); }
+    B& wName(QString name) { ItemBuilder<B,T>::mItemBlueprint.mName = name; return static_cast<B&>(*this);}
 };
 
 class Game : public BasicItem
 {
-    template <typename B, typename T, ENABLE_IF2(std::is_base_of<Game, T>)>
+    template <typename B, typename T, QX_ENABLE_IF_D(std::is_base_of<Game, T>)>
     friend class GameBuilder;
 //-Class Variables--------------------------------------------------------------------------------------------------
 protected:
@@ -105,17 +105,17 @@ public:
     QString getPlatform() const;
 };
 
-template <typename B, typename T, ENABLE_IF(std::is_base_of<Game, T>)>
+template <typename B, typename T, QX_ENABLE_IF(std::is_base_of<Game, T>)>
 class GameBuilder : public BasicItemBuilder<B, T>
 {
 //-Instance Functions------------------------------------------------------------------------------------------
 public:
-    B& wPlatform(QString platform) { ItemBuilder<B,T>::mItemBlueprint.mPlatform = platform; return *this; }
+    B& wPlatform(QString platform) { ItemBuilder<B,T>::mItemBlueprint.mPlatform = platform; return static_cast<B&>(*this); }
 };
 
 class AddApp : public BasicItem
 {
-    template <typename B, typename T, ENABLE_IF2(std::is_base_of<AddApp, T>)>
+    template <typename B, typename T, QX_ENABLE_IF_D(std::is_base_of<AddApp, T>)>
     friend class AddAppBuilder;
 //-Instance Variables-----------------------------------------------------------------------------------------------
 protected:
@@ -131,18 +131,18 @@ public:
     QUuid getGameID() const;
 };
 
-template <typename B, typename T, ENABLE_IF(std::is_base_of<AddApp, T>)>
+template <typename B, typename T, QX_ENABLE_IF(std::is_base_of<AddApp, T>)>
 class AddAppBuilder : public BasicItemBuilder<B, T>
 {
 //-Instance Functions------------------------------------------------------------------------------------------
 public:
-    B& wGameId(QString rawGameId) { ItemBuilder<B,T>::mItemBlueprint.mGameId = QUuid(rawGameId); return *this; }
+    B& wGameId(QString rawGameId) { ItemBuilder<B,T>::mItemBlueprint.mGameId = QUuid(rawGameId); return static_cast<B&>(*this); }
     B& wGameId(QUuid gameId) { ItemBuilder<B,T>::mItemBlueprint.mGameId = gameId; return *this; }
 };
 
 class PlaylistHeader : public BasicItem
 {
-    template <typename B, typename T, ENABLE_IF2(std::is_base_of<PlaylistHeader, T>)>
+    template <typename B, typename T, QX_ENABLE_IF_D(std::is_base_of<PlaylistHeader, T>)>
     friend class PlaylistHeaderBuilder;
 //-Constructor-------------------------------------------------------------------------------------------------
 protected:
@@ -150,13 +150,13 @@ protected:
     PlaylistHeader(QUuid id, QString name);
 };
 
-template <typename B, typename T, ENABLE_IF(std::is_base_of<PlaylistHeader, T>)>
+template <typename B, typename T, QX_ENABLE_IF(std::is_base_of<PlaylistHeader, T>)>
 class PlaylistHeaderBuilder : public BasicItemBuilder<B, T>
 {};
 
 class PlaylistGame : public BasicItem
 {
-    template <typename B, typename T, ENABLE_IF2(std::is_base_of<PlaylistGame, T>)>
+    template <typename B, typename T, QX_ENABLE_IF_D(std::is_base_of<PlaylistGame, T>)>
     friend class PlaylistGameBuilder;
 //-Instance Variables-----------------------------------------------------------------------------------------------
 protected:
@@ -171,14 +171,14 @@ public:
     QUuid getGameID() const;
 };
 
-template <typename B, typename T, ENABLE_IF(std::is_base_of<PlaylistGame, T>)>
+template <typename B, typename T, QX_ENABLE_IF(std::is_base_of<PlaylistGame, T>)>
 class PlaylistGameBuilder : public BasicItemBuilder<B, T>
 {
 //-Instance Functions------------------------------------------------------------------------------------------
 public:
     // These reuse the main ID on purpose, in this case gameId is a proxy for Id
-    B& wGameId(QString rawGameId) { ItemBuilder<B,T>::mItemBlueprint.mId = QUuid(rawGameId); return *this; }
-    B& wGameId(QUuid gameId) { ItemBuilder<B,T>::mItemBlueprint.mId = gameId; return *this; }
+    B& wGameId(QString rawGameId) { ItemBuilder<B,T>::mItemBlueprint.mId = QUuid(rawGameId); return static_cast<B&>(*this); }
+    B& wGameId(QUuid gameId) { ItemBuilder<B,T>::mItemBlueprint.mId = gameId; return static_cast<B&>(*this); }
 };
 
 }
